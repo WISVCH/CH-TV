168d1a827958868a7f5693e442ffed47536a4456
root <root@ubuntu-512mb-ams2-01.localdomain>
2017-04-03 15:15:13 +0000

    Admin frontend styling

--- app/models/image_content.rb
+++ app/models/image_content.rb
@@ -1,7 +1,7 @@
 class ImageContent < ApplicationRecord
   has_one :slide, as: :content
 
-  has_attached_file :image, styles: { thumb: "100x100#" },
+  has_attached_file :image, styles: { thumb: "240x135#", medium: "960x540#", fullscreen: "1920x1080#" },
                             convert_options: { :thumb => "-strip" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
